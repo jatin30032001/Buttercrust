de43e7f7eaee1ffdfa7bcb63123660e61cdc883a
jatin30032001 <jrsaini2090@gmail.com>
2023-01-24 11:04:29 +0530

    Admin Commit

--- buttercrust/pages/api/products/[id].js
+++ buttercrust/pages/api/products/[id].js
@@ -20,7 +20,7 @@ export default async function handler(req, res) {
 
   if (method === "PUT") {
     try {
-      const product = await Product.create(req.body);
+      const product = await Product.find(req.body);
       res.status(201).json(product);
     } catch (err) {
       res.status(500).json(err);
@@ -29,8 +29,8 @@ export default async function handler(req, res) {
 
   if (method === "DELETE") {
     try {
-      const product = await Product.create(req.body);
-      res.status(201).json(product);
+       await Product.findByIdAndDelete(id);
+      res.status(200).json("The Product has been deleted");
     } catch (err) {
       res.status(500).json(err);
     }
